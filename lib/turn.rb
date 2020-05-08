67c0a9af5522d09cd30393676d1c44f11602f740
Kelsey Dickson <kelseymedved@gmail.com>
2020-05-08 11:19:28 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,6 +6,6 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(input)
-  input.to_i - 1
+def valid_move?(board,index)
+  index.between(0,8) && !position_taken?(board, index)
 end
